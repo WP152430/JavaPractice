30bff01a444f325373c1f316d7740439a48fa7ed
정화령(1) <(1)@DESKTOP-7EPJ007>
2017-03-10 11:07:27 +0900

    실습과제1. 아이유 프로필

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -1,18 +1,23 @@
 package org.dimigo.basic;
 
+/**
+ * 
+ * <pre> 
+ * ${package_name}
+ *   |_ ${type_name}
+ * 1. 개요 : 
+ * 2. 작성일 : ${date}
+ *
+ *</pre>
+ *
+ * @author ${user}
+ * @version : 1.0
+ */
+
 public class PrimitiveDataType {
 
 	public static void main(String[] args) {
-//		- 이름(name, String), 성별(isMale, boolean), 나이(age, int)
-//		- 키(height, double), 몸무게(weight, float), 혈액형(bloodType, char)
-		
-//		<< 아이유 프로필 >>
-//		이름 : 아이유
-//		성별 : 여자
-//		나이 : 25세
-//		키 : 161.8cm
-//		몸무게 : 44.3kg
-//		혈액형 : A형
+
 		String name="아이유";
 		boolean isMale=false;
 		int age=25;
