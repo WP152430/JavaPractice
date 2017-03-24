1d11795375a533ba62a1436c8f624587b5c69621
정화령(1) <(1)@DESKTOP-7EPJ007>
2017-03-24 11:48:33 +0900

    커밋

---
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -0,0 +1,53 @@
+package org.dimigo.oop;
+
+public class Car2 {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	Car2(){
+		
+	}
+	Car2(String newCompany, String newModel,  String newColor, int newMaxSpeed,int newPrice){
+		company=newCompany;
+		color=newColor;
+		maxSpeed=newMaxSpeed;
+		price=newPrice;
+		model=newModel;
+		
+	}
+	
+	public String getCompany(){
+		return company;
+	}
+	public String getModel(){
+		return model;
+	}
+	public String getColor(){
+		return color;
+	}
+	public int getMaxSpeed(){
+		return maxSpeed;
+	}
+	public int getPrice(){
+		return price;
+	}
+	
+	public void setCompany(String newCompany){
+		company=newCompany;
+	}
+	public void setModel(String newModel){
+		model=newModel;
+	}
+	public void setColor(String newColor){
+		color=newColor;
+	}
+	public void setMaxSpeed(int newMaxSpeed){
+		maxSpeed=newMaxSpeed;
+	}
+	public void setPrice(int newPrice){
+		price=newPrice;
+	}
+}
