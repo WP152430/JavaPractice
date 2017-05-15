c13fc42a89218ed01fb1333ed0a026944c1d3824
정화령 <@DESKTOP-PUEU7I7>
2017-05-15 09:43:55 +0900

    커밋

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,19 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		SmartPhone[] sp ={new IPhone("iPhone 7", "애플",900000),
+				new Galaxy("갤럭시 S8", "삼성",800000)};
+		for(SmartPhone s : sp){
+			System.out.println(s);
+			s.turnOn();
+			s.pay();
+			s.useSpecialFunction(s);
+			s.turnOff();
+			System.out.println("------------------------------------------");
+			
+		}
+	}
+
+}
